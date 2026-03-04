65cef04479a9608c8b549e1dcd6b563f86f0b87a
madiravijay402 <vijaymadira681@gmail.com>
2026-03-04 21:41:50 +0530

    navbar

--- Day2-React/src/components/NavBar.jsx
+++ Day2-React/src/components/NavBar.jsx
@@ -2,7 +2,15 @@ import React from "react";
 const NavBar=()=>{
     return(
         <>
-            
+            <div>
+                <a href="https://youtu.be/fJaAYcERf3Y?si=AOXGqy68w-IkYh4g">trailer</a>
+            </div>
+            <div>
+                <a href="https://open.spotify.com/playlist/0rES2Byrozz9TnaUTKAsWo?si=33091830d92d4981">album</a>
+            </div>
+            <div>
+                <a href="https://www.kvnproductions.co.in/">contact</a>
+            </div>
         </>
     )
 }
