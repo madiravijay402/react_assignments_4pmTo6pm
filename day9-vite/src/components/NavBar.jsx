a2a8414b89ce2bc81fabb3bdadd3de2796bdb671
madiravijay402 <vijaymadira681@gmail.com>
2026-03-13 21:04:56 +0530

    commit

---
+++ day9-vite/src/components/NavBar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <>
+        <div>
+            <div>
+                <img src="" alt="" />
+            </div>
+            <div>
+                <h1></h1>
+            </div>
+            <div>
+                
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default NavBar
