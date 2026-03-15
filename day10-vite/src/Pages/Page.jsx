fb21bb3b33b4530237c7bb604a3cde727134f516
madiravijay402 <vijaymadira681@gmail.com>
2026-03-15 13:23:58 +0530

    update

--- day10-vite/src/Pages/Page.jsx
+++ day10-vite/src/Pages/Page.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Task1 from '../components/Task1'
 import Task2 from '../components/Task2'
 import Task3 from '../components/Task3'
+import Task5 from '../components/Task5'
 
 const Page = () => {
   return (
@@ -9,6 +10,7 @@ const Page = () => {
         <Task1/>
         <Task2/>
         <Task3/>
+        <Task5/>
     </>
   )
 }
--- day10-vite/src/components/Task5.jsx
+++ day10-vite/src/components/Task5.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Task5 = () => {
+  const [profile, setProfile] = useState({
+    name: "Priya",
+    role: "Developer",
+    experience: 2
+  });
+
+  const promote = () => {
+    setProfile({
+      ...profile,
+      role: "Senior Developer",
+      experience: profile.experience + 1
+    });
+  };
+
   return (
-    <div>Task5</div>
-  )
+    <div className='bg-purple-500 flex flex-col gap-5 justify-center p-5'>
+        <div>
+             <h2>Name: {profile.name}</h2>
+      <h2>Role: {profile.role}</h2>
+      <h2>Experience: {profile.experience}</h2>
+        </div>
+<div>
+    <button onClick={promote} className='bg-blue-500 rounded-2xl p-3 '>Promote</button>
+</div>
+      
+    </div>
+  );
 }
 
 export default Task5
