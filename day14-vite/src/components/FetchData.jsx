df9dea0307111a740c8ef7d756d3376387b0f9ef
madiravijay402 <vijaymadira681@gmail.com>
2026-03-22 18:39:54 +0530

    commit

--- day14-vite/src/components/FetchData.jsx
+++ day14-vite/src/components/FetchData.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const FetchData = () => {
+    const[users,setUsers]=useState([]);
   return (
     <>
-        
+
     </>
   )
 }
