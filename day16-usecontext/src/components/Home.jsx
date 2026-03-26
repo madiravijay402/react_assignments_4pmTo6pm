d2be8f9f34776c727ed3c1c9cd7f45d36c5aab65
madiravijay402 <vijaymadira681@gmail.com>
2026-03-26 11:36:57 +0530

    commit

--- day16-usecontext/src/components/Home.jsx
+++ day16-usecontext/src/components/Home.jsx
@@ -3,12 +3,28 @@ import Countcontext from '../createcontext/Countcontext'
 
 const Home = () => {
     const{toggle}=useContext(Countcontext)
+    const{data,handlechange,decrease}=useContext()
   return (
     <>
     <div>
+      <div>
         {toggle?<div className='bg-black w-750 text-white h-550 text-center'></div>:<div className='bg-white w-750 text-black h-550 text-center'></div>}
         {toggle?<p>dark</p>:<p>light</p>}
     </div>
+    <div className='bg-black text-white flex flex-col justify-center items-center p-2 gap-3'>
+      <div className='bg-yellow-400 text-black rounded p-2'>
+        logo
+      </div>
+      <div className='bg-red-700 text-green-700 rounded-2xl p-2'>
+        {data}
+      </div>
+      <div className='flex justify-center bg-red-200 rounded-2xl '>
+        <button onClick={handlechange} className='text-red'>+</button>
+        <button onClick={decrease} className='text-red'>-</button>
+      </div>
+    </div>
+    </div>
+    
     </>
   )
 }
--- day16-usecontext/src/provider/CountProvider.jsx
+++ day16-usecontext/src/provider/CountProvider.jsx
@@ -1,16 +1,19 @@
-// import React, { useState } from 'react'
-// import Countcontext from '../createcontext/Countcontext';
+import React, { useState } from 'react'
+import Countcontext from '../createcontext/Countcontext';
 
-// const CountProvider = ({children}) => {
-//     const[data,setData]=useState(0);
-//     const handlechange=()=>{
-//         setData((pre)=>pre+1)
-//     }
-//   return (
-//     <Countcontext.Provider value={{handlechange,data}}>
-//         {children}
-//     </Countcontext.Provider>
-//   )
-// }
+const CountProvider = ({children}) => {
+    const[data,setData]=useState(0);
+    const handlechange=()=>{
+        setData((pre)=>pre+1)
+    }
+    const decrease=()=>{
+      setData((pre)=>pre-1)
+    }
+  return (
+    <Countcontext.Provider value={{handlechange,data,decrease}}>
+        {children}
+    </Countcontext.Provider>
+  )
+}
 
-// export default CountProvider
+export default CountProvider
